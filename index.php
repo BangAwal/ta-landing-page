0c9f4ebae4add2a343368738b38ee4bcd935bccc
awl.ddn <muhammadawaldin@gmail.com>
2021-10-18 12:01:08 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Masterparent</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Ninestars - v4.5.0
+  * Template URL: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1 class="text-light"><a href=""><span>Masterparent</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About Us</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="">Login</a></li>
+          <li><a class="getstarted scrollto" href="">Daftar Sekarang!</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row gy-4">
+        <div class="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1>Masterparent.id</h1>
+          <h2>Kami adalah teman bagi Parent dalam mengembangkan karakter sosial-emosional Si Kecil yang berusia 0-6 tahun dengan berbagai kegiatan seru dan menyenangkan.</h2>
+          <div>
+            <a href="#about" class="btn-get-started scrollto">Daftar Sekarang!</a>
+          </div>
+        </div>
+        <div class="col-lg-6 order-1 order-lg-2 hero-img">
+          <img src="assets/img/hero-img.svg" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row justify-content-between">
+          <div class="col-lg-5 d-flex align-items-center justify-content-center about-img">
+            <img src="assets/img/about-img.svg" class="img-fluid" alt="" data-aos="zoom-in">
+          </div>
+          <div class="col-lg-6 pt-5 pt-lg-0">
+            <div class="row">
+              <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
+              <img src="assets/img/mengapa-usia.png" alt="" style="width:50px;">                
+<h4>Mengapa Sejak Usia 0-6 Tahun?</h4>
+
+                <p>Berbagai penelitian mengungkapkan bahwa pertumbuhan otak berlangsung dengan kecepatan yang tinggi. Bisa dikatakan bahwa Si Kecil yang sedang berada pada usia golden age (0-6 tahun) memiliki potensi yang besar sebanyak 90% perkembangan otaknya terjadi pada masa ini.</p>
+              </div>
+              <div class="col-md-6" data-aos="fade-up" data-aos-delay="200">
+              <img src="assets/img/mengapa-sosio-emosional.png" alt="" style="width:50px;">
+                <h4>Mengapa Karakter Sosial-Emosional?</h4>
+
+                <p>Menurut  Daniel Goleman, 80% penyumbang kesuksesan adalah  kecerdasan  emosional  atau  Emotional  Quotient  (EQ)  seperti kemampuan  memotivasi  diri  sendiri,  mengatasi  frustasi,  mengontrol  desakan  hati, mengatur  suasana  hati (mood),  berempati serta kemampuan  bekerja  sama. Jika Parent mengembangkan karakter sosial-emosional sejak masa golden age, maka otak Si Kecil akan lebih mudah dalam menerima karena selaras dengan perkembangan otaknya yang cepat, dan bahkan mampu  menyesuaikan keadaan emosional dalam berbagai situasi di lingkungan sosialnya sampai kelak Ia tumbuh dewasa.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <p>Penawaran Kami</p>
+        </div>
+
+        <div class="row">
+          <div class="col d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box">
+              <div class="icon">
+                <img src="assets/img/milestone.png" alt="" style="width:50px;">
+</div>
+              <h4 class="title">Milestone Karakter</h4>
+              <p class="description">Fitur ini memberikan informasi karakter yang sesuai dengan usia Si Kecil. Dengan berbagai informasi tersebut, Parent dapat memantau perkembangan Si Kecil melalui masterparent.id.</p>
+            </div>
+          </div>
+
+          <div class="col d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box">
+            <div class="icon">
+                <img src="assets/img/rekomendasi-aktivitas.png" alt="" style="width:50px;">
+</div>              
+        <h4 class="title">Rekomendasi Aktivitas</h4>
+              <p class="description">Fitur ini menyediakan rekomendasi ide aktivitas dan permainan yang membantu Parent dalam mengembangkan karakter Si Kecil.</p>
+            </div>
+          </div>
+
+          <div class="col d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box">
+            <div class="icon">
+                <img src="assets/img/bagaimana-hari-ini.png" alt="" style="width:50px;">
+</div>
+<h4 class="title">Bagaimana Hari Ini?</h4>
+              <p class="description">Fitur ini memberikan pertanyaan refleksi diri tentang  pola asuh yang telah dilakukan pada hari tersebut.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
+  </main><!-- End #main -->
+
+  <!-- ======= Clients Section ======= -->
+  <section id="clients" class="clients section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <p>Didukung Oleh</p>
+        </div>
+
+
+        <div class="text-center">
+        <img src="assets/img/KampusMerdeka.png" class="rounded" alt="" style="width:300px;">
+          <img src="assets/img/Cias.png" class="rounded" alt="" style="width:300px;">
+        </div>
+        
+
+      </div>
+    </section><!-- End Clients Section -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container py-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Masterparent</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/ninestars-free-bootstrap-3-theme-for-creative/ -->
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
